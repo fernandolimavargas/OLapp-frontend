12f6ecd292541ba896f9f038cf694d1ea64e78df
Fernando Vargas <fervargas2210@gmail.com>
2026-06-23 07:48:02 -0300

    UPD - Configurado o servidor do railway (backend)

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://localhost:7151",
+  //baseURL: "https://localhost:7151",
+  baseURL: import.meta.env.VITE_API_URL
 });
